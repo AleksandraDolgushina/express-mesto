fed107454083a6c5687cdcb49800fd079b0b41cf
Aleksandra Dolgushina <sashadolgushina@yandex.ru>
2021-07-06 13:02:19 +0300

    готовый  проект

--- controllers/users.js
+++ controllers/users.js
@@ -116,7 +116,7 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'secret-key', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
       res.cookie('jwt', token, {
         httpOnly: true,
         sameSite: true,
